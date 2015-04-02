9e6d97165e45d51099787a0e17132b3ec5a2ebd9
Denis P <lsden1995@gmail.com>
2015-04-02 15:47:36 +0300

    Add repository and uow

---
+++ MSHRCS.BusinessLogic/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NoTime.BusinessLogic.DataModel;
+
+namespace NoTime.BusinessLogic.UnitOfWork
+{    
+    public interface IUnitOfWork : IDisposable
+    {
+		EntityContainer Context { get; }
+		NT_BlogEntities BlogContext { get; }
+        void Commit();        
+    }
+}
---
+++ MSHRCS.BusinessLogic/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using NoTime.BusinessLogic.DataModel;
+
+namespace NoTime.BusinessLogic.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+		private readonly EntityContainer _context;
+		private readonly NT_BlogEntities _blogContext;
+
+		public UnitOfWork(EntityContainer context, NT_BlogEntities blogContext)
+        {
+            _context = context;
+			_blogContext = blogContext;
+        }
+
+		public EntityContainer Context
+        {
+            get { return _context; }
+        }
+
+		public NT_BlogEntities BlogContext
+		{
+			get { return _blogContext; }
+		}
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+	        _blogContext.SaveChanges();
+        }
+       
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+
+	        if (_blogContext != null)
+	        {
+		        _blogContext.Dispose();
+	        }
+        }
+    }
+}
